2b0c4499b925fdee437a8a40e424f4541fff01d7
Lucas <lucasnrgaard@gmail.com>
2025-04-20 18:24:21 +0200

    chore: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bumplings-test
+
+This is a repository for testing [bumplings](https://github.com/luxass/bumplings).
---
+++ bumplings.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from "../bumplings/src/config";
+
+export default defineConfig({
+  mode: "monolith",
+});
